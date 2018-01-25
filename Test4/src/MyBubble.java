f3ee0362926f36d20171f3311720cf378af8af85
Whiteheron819 <Las_os@mail.ru>
2018-01-25 18:42:03 +0300

    Задача по пузырьковой сортировке. Добавлен ввод размера массива и генератор случайных чисел.

--- Test4/src/MyBubble.java
+++ Test4/src/MyBubble.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class MyBubble {
     public static void main(String[] args) {
 
+        int a, b, c;
+
         Random rand = new Random();
         System.out.println(" Укажите размер массива: ");
         Scanner in = new Scanner(System.in);
@@ -13,9 +15,21 @@ public class MyBubble {
 
 
         for (int i = 0; i < size; i++) {
-            int result = rand.nextInt(1500) + 1;
+            int result = rand.nextInt(1500);
             nums[i] = result;
         }
-        System.out.print(" Неосортированный массив: " + Arrays.toString(nums));
+        System.out.print(" Неотсортированный массив: " + Arrays.toString(nums));
+        System.out.println();
+
+        for(a = 1; a < size; a++)
+            for(b = size - 1; b >=a; b--) {
+                if(nums[b-1] > nums[b]) {
+
+                    c = nums[b-1];
+                    nums[b-1] = nums[b];
+                    nums[b] = c;
+                }
+            }
+        System.out.print(" Отсортированный массив: " + Arrays.toString(nums));
     }
 }
